1064480b00f3cbdb85be89304ae553728da8d365
macbook m1 <dungdv01852@gmail.com>
2024-06-26 14:48:20 +0700

    fsa

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" /><div>
+      <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
+      <form>
+        <input type="hidden" defaultValue={process.env.REACT_APP_NOT_SECRET_CODE} />
+      </form>
+    </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
